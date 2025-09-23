ba40cb32873673dfd9e4e570acc030f7d1d22d0f
Leon Shimizu <lmshimizu@gmail.com>
2025-09-23 16:45:30 +1000

    Add seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# test = Supplier.create(
+#   name: "Nike",
+#   email: "info@nike.com",
+#   phone: "(800)123-1234"
+# )
+
+# product = Product.create(
+#   title: "Kobe 6",
+#   price: 150,
+#   description: "Kobe's shoes",
+#   size: 12,
+#   supplier: test
+#   # supplier_id: supplier.id
+# )
+
+i = 1
+5.times do
+  supplier = Supplier.create(
+    name: Faker::Company.name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number
+  )
+
+  product = Product.create(
+    title: "Product #{i}",
+    price: i * 10,
+    description: "Product #{i}'s description",
+    size: i + 10,
+    supplier_id: supplier.id
+  )
+
+  puts "Created #{supplier.name}"
+  puts "Created #{product.title}"
+  i += 1
+end
+
+# puts "5 Suppliers created"
+
+
